aeedb397500d47e69afcd2e5bf574b316907a718
Rahul Rahate <rrahate@godaddy.com>
2025-03-03 11:41:45 -0600

    21

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rrahate-godaddy.github.io/datacoux',
+  site: 'https://rrahate-godaddy.github.io',
   base: '/datacoux',
   outDir: 'dist',
   integrations: [react()],
