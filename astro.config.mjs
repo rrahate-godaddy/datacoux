5001d5b55dce5fc18d8315ca202152046ce982e1
Rahul Rahate <rrahate@godaddy.com>
2025-03-03 05:08:56 -0600

    config the github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://rrahate-godaddy.github.io',
+  base: '/datacoux/',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
